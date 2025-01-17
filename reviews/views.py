560e28a544f5f34b68d3de5ad047bf70c235f269
Jaden Neal <jaden.neal142@gmail.com>
2025-01-16 19:42:53 -0600

    Mesages

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
-from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView, DeleteView,  DetailView, ListView, UpdateView
 )
@@ -8,9 +9,10 @@ from django.views.generic import (
 from .models import Review
 from .forms import ReviewForm
 
-class ReviewCreateView(LoginRequiredMixin, CreateView):
+class ReviewCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Review
     form_class = ReviewForm
+    success_message = 'Review created.'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -21,9 +23,18 @@ class ReviewDeleteView(UserPassesTestMixin, DeleteView):
     model = Review
     success_url = reverse_lazy('reviews:list')
 
+    def delete(self, request, *args, **kwargs):
+        result = super().delete(request, *args, **kwargs)
+        return result
+
     def test_func(self):
         obj = self.get_object()
         return self.request.user == obj.user
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Review deleted.')
+        return super().form_valid(form)
+
 
 
 class ReviewDetailView(DetailView):
@@ -34,9 +45,10 @@ class ReviewListView(ListView):
     model = Review
 
 
-class ReviewUpdateView(UserPassesTestMixin, UpdateView):
+class ReviewUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     model = Review
     form_class = ReviewForm
+    success_message = 'Review updated.'
 
     def test_func(self):
         obj = self.get_object()
